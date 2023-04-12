a19c9a02634117fe01503fa806634b8c85b4a94d
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2023-04-11 21:08:59 -0400

    Added public folder path

--- backend/models/server.js
+++ backend/models/server.js
@@ -1,46 +1,44 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
 
-    constructor() {
-        this.app = express();
-        this.port = process.env.PORT;
+    this.paths = {
+      send: "/api/send",
+    };
 
-        this.paths = {
-            send: '/api/send',
-        }
+    this.middlewares();
 
-        this.middlewares();
+    this.routes();
+  }
 
-        this.routes();
-    }
+  middlewares() {
+    this.app.use(cors());
 
+    this.app.use(express.json());
 
-    middlewares() {
+    const buildPath = path.join(__dirname, "../../frontend", "build");
+    this.app.use(express.static(buildPath));
 
-        this.app.use(cors());
+    this.app.get("*", (req, res) => {
+      const indexPath = path.join(buildPath, "index.html");
+      res.sendFile(indexPath);
+    });
+  }
 
-        this.app.use(express.json());
+  routes() {
+    this.app.use(this.paths.send, require("../routes/send"));
+  }
 
-        this.app.use(express.static('public'));
-
-    }
-
-    routes() {
-        this.app.use(this.paths.send, require('../routes/send'));
-    }
-
-    listen() {
-        this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto', this.port);
-        });
-    }
-
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en puerto", this.port);
+    });
+  }
 }
-
-
-
 
 module.exports = Server;
